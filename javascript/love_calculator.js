8a51e4752f5ca8984ec78e1266e1eb1f1c21fc9c
iffishells <iffishells@gmail.com>
2020-07-16 11:17:52 +0500

    Adding javascript code

---
+++ javascript/love_calculator.js
@@ -0,0 +1,5 @@
+var male_name = window.prompt("Enter the Male Name");
+var Female_name = window.prompt("Enter the Male Name");
+
+var love_percentage = math.random() *100;
+console.log(love_percentage)
