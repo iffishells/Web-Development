a44704e4695a732a8cf77d8c0f59472062f71307
iffishells <iffishells@gmail.com>
2020-09-17 21:38:19 +0500

    Added user information

--- Registration-Form/server.js
+++ Registration-Form/server.js
@@ -8,15 +8,21 @@ const App = Express();
 
 App.use(Express.static(Path.join(__dirname , 'public')));
 App.use(BodyParser.urlencoded({extended : true}));
+App.use(Express.static('public'));
 App.set('view engine' ,'ejs');
 
 
 
 App.get('/' , function(req, resp)
 {
-    resp.render("index")
+    resp.render("index.ejs")
 })
 
+// App.post('/', function(req, resp)
+// {
+    // 
+// })
+// 
 App.listen(3000,function(req, resp)
 {
     console.log("your server is listen at 3000 port");
